ef08dd2e7526e8c405afd5d6ced07510826c5d51
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-08-18 02:17:18 +0500

    Removed warnings

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const Person = props => {
     return React.createElement('div', {}, [
-        React.createElement('h1', {}, props.name),
-        React.createElement('p', {}, props.occupation),
+        React.createElement('h1', { key: 1 }, props.name),
+        React.createElement('p', { key: 2 }, props.occupation),
     ]);
 };
 
 const App = () => {
     return React.createElement('div', {}, [
-        React.createElement('h1', { class: 'title' }, 'react IS rendered'),
-        React.createElement(Person, { name: 'Mukarram', occupation: 'programmer' }, null),
-        React.createElement(Person, { name: 'Ali', occupation: 'developer' }, null),
-        React.createElement(Person, { name: 'Kazmi', occupation: 'engineer' }, null),
+        React.createElement('h1', { className: 'title', key: 3 }, 'react IS rendered'),
+        React.createElement(Person, { name: 'Mukarram', occupation: 'programmer', key: 4 }, null),
+        React.createElement(Person, { name: 'Ali', occupation: 'developer', key: 5 }, null),
+        React.createElement(Person, { name: 'Kazmi', occupation: 'engineer', key: 6 }, null),
     ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
